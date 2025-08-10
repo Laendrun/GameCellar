68f80f11e94e2e7909d787fdaa990b4c994a6def
Simon <simon.aeby@laendrun.ch>
2025-08-10 13:10:05 +0200

    fix nginx/entrypoint.sh

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -6,21 +6,15 @@ envsubst '${DOMAIN}' \
   < /etc/nginx/conf.d/default.conf.tpl \
   > /etc/nginx/conf.d/default.conf
 
-# 2) start nginx in background
 nginx -g 'daemon on;'
 
-# 3) obtain certs on first-run
-if [ ! -d "/etc/letsencrypt/live/${DOMAIN}" ]; then
-  certbot --nginx \
-    --noninteractive \
-    --agree-tos \
-    --redirect \
-    --email "${LETSENCRYPT_EMAIL}" \
-    -d "${DOMAIN}"
-fi
+certbot --nginx \
+  --noninteractive \
+  --agree-tos \
+  --redirect \
+  --email "${LETSENCRYPT_EMAIL}" \
+  -d "${DOMAIN}"
 
-# 4) gracefully stop the background nginx
+
 nginx -s quit
-
-# 5) exec nginx in the foreground
 exec nginx -g 'daemon off;'
