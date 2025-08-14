41c67547257d64ced8cdd826ec8d01a42ead7a5e
Simon <simon.aeby@laendrun.ch>
2025-08-14 22:46:28 +0200

    remove migrations files

--- backend/prisma/migrations/20250615143135_add_unique_translation_constraint/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[gameId,lang]` on the table `Translation` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Translation_gameId_lang_key" ON "Translation"("gameId", "lang");
